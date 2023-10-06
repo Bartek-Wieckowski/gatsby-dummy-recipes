11d9b715669568e844ba793ab94ab9e5a619563f
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-10-06 16:50:31 +0200

    used components

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from "react";
+import Layout from "../components/Layout";
 
 export default function about() {
   return (
-    <div>about</div>
-  )
+    <Layout>
+      <div>about</div>
+    </Layout>
+  );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,12 @@
 import React from "react";
-import { Link } from "gatsby";
+import Layout from "../components/Layout";
 
-export default function index() {
-  return <div>test</div>;
+export default function Home() {
+  return (
+    <div className="app">
+      <Layout>
+        <h1>Hello peopleeee</h1>
+      </Layout>
+    </div>
+  );
 }
