780c65c459849dfaff846e710671afbd28c891e7
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-10-08 12:20:26 +0200

    created first graphql query and pass this var to props

--- src/components/AllRecipes.jsx
+++ src/components/AllRecipes.jsx
@@ -1,13 +1,34 @@
 import React from "react";
 import TagsList from "./TagsList";
 import RecipesList from "./RecipesList";
+import { useStaticQuery, graphql } from "gatsby";
+
+export const query = graphql`
+  query {
+    allContentfulRecipe(sort: { title: ASC }) {
+      nodes {
+        id
+        title
+        cookTime
+        prepTime
+        content {
+          tags
+        }
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+        }
+      }
+    }
+  }
+`;
 
 export default function AllRecipes() {
+  const data = useStaticQuery(query);
+  const recipes = data.allContentfulRecipe.nodes;
   return (
-    <div>
-      <h4>all recipes</h4>
-      <TagsList />
-      <RecipesList />
-    </div>
+    <section className="recipes-container">
+      <TagsList recipes={recipes} />
+      <RecipesList recipes={recipes} />
+    </section>
   );
 }
