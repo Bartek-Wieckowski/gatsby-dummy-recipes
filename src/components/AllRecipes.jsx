42b096c89c88d12989902e6643c6f8debbac66b5
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-10-08 11:56:26 +0200

    created components and used them in Pages

---
+++ src/components/AllRecipes.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TagsList from "./TagsList";
+import RecipesList from "./RecipesList";
+
+export default function AllRecipes() {
+  return (
+    <div>
+      <h4>all recipes</h4>
+      <TagsList />
+      <RecipesList />
+    </div>
+  );
+}
--- src/pages/recipes.jsx
+++ src/pages/recipes.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Layout from "../components/Layout";
+import AllRecipes from "../components/AllRecipes";
 
 export default function Recipes() {
   return (
     <Layout>
-      <div>Recipes page</div>
+      <main className="page">
+        <AllRecipes/>
+      </main>
+
     </Layout>
   );
 }
