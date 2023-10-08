61e781b4d3b3c6481830b781f85aa4dda88145b4
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-10-08 12:21:31 +0200

    the component has been filled with data

--- src/components/RecipesList.jsx
+++ src/components/RecipesList.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { Link } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import React from "react";
 
-export default function RecipesList() {
+export default function RecipesList({ recipes = [] }) {
   return (
-    <div>RecipesList</div>
-  )
+    <div className="recipes-list">
+      {recipes.map((recipe) => {
+        const { id, title, image, prepTime, cookTime } = recipe;
+        const pathToImage = getImage(image);
+        return (
+          <Link key={id} to={`/${title}`} className="recipe">
+            <GatsbyImage image={pathToImage} className="recipe-img" alt={title} />
+            <h5>{title}</h5>
+            <p>
+              Prep : {prepTime}min | Cook: {cookTime}min
+            </p>
+          </Link>
+        );
+      })}
+    </div>
+  );
 }
