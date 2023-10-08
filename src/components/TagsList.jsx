e781cac4b700992041ec46e11f0eb5030275ae78
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-10-08 13:05:58 +0200

    used setupTags function and map text,values in jsx

--- src/components/TagsList.jsx
+++ src/components/TagsList.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
+import setupTags from "../utils/setupTags";
+import { Link } from "gatsby";
 
-export default function TagsList() {
+export default function TagsList({ recipes }) {
+  const newTags = setupTags(recipes);
   return (
-    <div>TagsList</div>
-  )
+    <div className="tag-container">
+      <h4>recipes</h4>
+      <div className="tags-list">
+        {newTags.map((tag, index) => {
+          const [text, value] = tag;
+          return (
+            <Link key={index} to={`/${text}`}>
+              {text} ({value})
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
