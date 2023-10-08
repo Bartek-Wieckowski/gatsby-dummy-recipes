8eb66125183f72f3c4e173f24fcfc085d6f8ec21
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-10-08 12:34:57 +0200

    used graphql query, used recipelist with pass props to display featured recipes list

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 import Layout from "../components/Layout";
 import { StaticImage } from "gatsby-plugin-image";
-import { Link } from "gatsby";
+import { Link, graphql } from "gatsby";
+import RecipesList from "../components/RecipesList";
 
-export default function About() {
+export const query = graphql`
+  query {
+    allContentfulRecipe(sort: { title: ASC }, filter: { featured: { eq: true } }) {
+      nodes {
+        id
+        title
+        cookTime
+        prepTime
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+        }
+      }
+    }
+  }
+`;
+
+export default function About({ data }) {
+  const recipes = data.allContentfulRecipe.nodes
   return (
     <Layout>
       <main className="page">
@@ -29,6 +47,10 @@ export default function About() {
             className="about-img"
             placeholder="blurred"
           />
+        </section>
+        <section className="featured-recipes">
+          <h5>Look at this Awesomesouce!</h5>
+          <RecipesList recipes={recipes} />
         </section>
       </main>
     </Layout>
