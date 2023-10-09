23df19c26a3281f74ee03ffa0edbca40e895c74f
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-10-09 13:43:34 +0200

    used slugify in path

--- src/pages/tags.jsx
+++ src/pages/tags.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Layout from "../components/Layout";
 import { graphql, Link } from "gatsby";
 import setupTags from "../utils/setupTags";
+import slugify from "slugify";
 
 export const query = graphql`
   query {
@@ -18,14 +19,16 @@ export const query = graphql`
 export default function Tags({ data }) {
   const tags = data.allContentfulRecipe.nodes;
   const newTags = setupTags(tags);
+  
   return (
     <Layout>
       <main className="page">
         <section className="tags-page">
           {newTags.map((tag, index) => {
             const [text, value] = tag;
+            const slug = slugify(text, { lower: true });
             return (
-              <Link to={`/${text}`} key={index} className="tag">
+              <Link to={`/tags/${slug}`} key={index} className="tag">
                 <h5>{text}</h5>
                 <p>{value} recipe</p>
               </Link>
